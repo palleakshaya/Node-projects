d8060e5439570c7208655866951fcc0257df9357
palleakshaya <akshaya.palle@proclink.com>
2024-08-09 15:32:00 +0530

    CRUD operations done

---
+++ class.md
@@ -0,0 +1,12 @@
+### DataTypes in DynamoDb
+
+1. Scalar
+   - string
+   - number
+   - boolean
+2. Document
+   - list(array)
+   - map(stats in movie example) -> key values{}
+3. Set
+   - set of strings
+   - set of numbers
---
+++ entities/moviesentity.js
@@ -0,0 +1,53 @@
+import { Entity } from "electrodb"; //ORM(object relational mapping -> SDK is very hard to use so to make it easier, electrodb is used)
+import { client } from "../util/dbconnections.js";
+//client is the live connection to AWS
+
+const Movies = new Entity(
+  {
+    model: {
+      entity: "Movies",
+      version: "2",
+      service: "MovieService",
+    },
+    attributes: {
+      movieId: {
+        type: "string",
+      },
+      name: {
+        type: "string",
+      },
+      poster: {
+        type: "string",
+        // required: true,
+      },
+      rating: {
+        type: "number",
+        required: true,
+      },
+      trailer: {
+        type: "string",
+      },
+      summary: {
+        type: "string",
+      },
+    },
+    indexes: {
+      primary: {
+        pk: {
+          // highlight-next-line
+          field: "pk",
+          facets: ["movieId"],
+        },
+        sk: {
+          // highlight-next-line
+          field: "sk",
+          facets: [],
+        },
+      },
+    },
+    // add your DocumentClient and TableName as a second parameter
+  },
+  { client, table: "movies" }
+);
+
+export { Movies };
